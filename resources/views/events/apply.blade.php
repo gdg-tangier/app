5c76b23287d485b21c4e42df9b696bc198ee83ee
Unknown <souhail.merroun@hotmail.com>
2018-02-24 23:08:41 +0000

    Access skills + code invitation view

--- resources/views/events/apply.blade.php
+++ resources/views/events/apply.blade.php
@@ -4,17 +4,48 @@
 
 	<div class="container">
         <div class="row">
-        	<div class="col-md-12">
-        		<ol class="breadcrumb">
-				  	<li class="breadcrumb-item"><a href="/home">Home</a></li>
-				  	<li class="breadcrumb-item">{{ $event->title }}</li>
-				  	<li class="breadcrumb-item active">Apply</li>
-				</ol>
-        	</div>
-        </div>
-        <div class="row">
-            <div class="col-md-8">
-            	//Here you can apply
+            <div class="col-md-3">
+                @component('card', ['title' => $event->title])
+
+                    //Basic events informations
+
+                @endcomponent
+            </div>
+            <div class="col-md-9">
+                <div id="accordion">
+                    <div class="card">
+                        <div class="card-header">
+                          <h5 class="mb-0">
+                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne">
+                                I don't know If this event will benefit me? Am I skilled enough? Will I benefit or It will be too hard?
+                            </button>
+                          </h5>
+                        </div>
+                        <div id="collapseOne" class="collapse" data-parent="#accordion">
+                            <div class="card-body">
+                                You can use this quiz to assess your skills and know if you are eligible to assist this event.<hr> 
+
+                                <a href="#" class="btn btn-primary">Assess my skills</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                          <h5 class="mb-0">
+                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo">
+                                I want to assist this event, I am ready! :)
+                            </button>
+                          </h5>
+                        </div>
+                        <div id="collapseTwo" class="collapse" data-parent="#accordion">
+                            <div class="card-body">
+                                Click on the button down below to get your invitation code. <hr>
+
+                                <a href="#" class="btn btn-success">I want my invitation code</a>
+                            </div>
+                        </div>
+                    </div>      
+                </div>
            	</div>
         </div>
 	</div>
