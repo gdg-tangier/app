7e9d0a838fccef415aad4e688980a7e4c8b5e25d
Unknown <souhail.merroun@hotmail.com>
2018-02-24 21:55:01 +0000

    Add links

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,29 @@
 @section('content')
 
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 @component('card', ['title' => 'Dashboard'])
 
-                    You are logged in!
+					
+
+                @endcomponent
+            </div>
+
+            <div class="col-md-4">
+                @component('card', ['title' => 'Links'])
+
+					<ul>
+						<li><a href="https://github.com/gdg-tangier" target="_blank">Github</a></li>
+						<li><a href="https://gdg-tangier.slack.com" target="_blank">Slack</a></li>
+						<li><a href="https://www.meetup.com/gdgtangier/" target="_blank">Meetup</a></li>
+					</ul>
+
+					<ul>
+						<li><a href="https://www.facebook.com/gdgtangier/" target="_blank">Facebook</a></li>
+						<li><a href="https://twitter.com/gdgtangier" target="_blank">Twitter</a></li>
+						<li><a href="https://www.instagram.com/gdgtangier/" target="_blank">Instagram</a></li>
+					</ul>
 
                 @endcomponent
             </div>
