62270506394461077d9953d9b88548ae20ef525c
Unknown <souhail.merroun@hotmail.com>
2018-02-24 23:10:43 +0000

    Skill assesment before event

--- database/migrations/2018_02_24_220433_create_events_table.php
+++ database/migrations/2018_02_24_220433_create_events_table.php
@@ -17,6 +17,7 @@ class CreateEventsTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->string('url');
+            $table->string('assessment')->nullable();
             $table->datetime('scheduled_at');
         });
     }
--- resources/views/events/apply.blade.php
+++ resources/views/events/apply.blade.php
@@ -13,22 +13,24 @@
             </div>
             <div class="col-md-9">
                 <div id="accordion">
-                    <div class="card">
-                        <div class="card-header">
-                          <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne">
-                                I don't know If this event will benefit me? Am I skilled enough? Will I benefit or It will be too hard?
-                            </button>
-                          </h5>
-                        </div>
-                        <div id="collapseOne" class="collapse" data-parent="#accordion">
-                            <div class="card-body">
-                                You can use this quiz to assess your skills and know if you are eligible to assist this event.<hr> 
+                    @if($event->assessment)
+                        <div class="card">
+                            <div class="card-header">
+                              <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne">
+                                    I don't know If this event will benefit me? Am I skilled enough? Will I benefit or It will be too hard?
+                                </button>
+                              </h5>
+                            </div>
+                            <div id="collapseOne" class="collapse" data-parent="#accordion">
+                                <div class="card-body">
+                                    You can use this quiz to assess your skills and know if you are eligible to assist this event.<hr> 
 
-                                <a href="#" class="btn btn-primary">Assess my skills</a>
+                                    <a href="{{ $event->assessment }}" target="_blank" class="btn btn-primary">Assess my skills</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                     <div class="card">
                         <div class="card-header">
                           <h5 class="mb-0">
