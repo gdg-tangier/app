a7dba0349deb818127acca3d25d259489e5b14a6
Unknown <souhail.merroun@hotmail.com>
2018-02-25 00:42:43 +0000

    Homepage

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -5,9 +5,19 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                @component('card', ['title' => 'Homepage'])
+                @component('card', ['title' => 'Welcome to GDG Tangier'])
 
-                    //
+                	Grow your skills and build a career as: 
+
+                	<hr>
+
+					Mobile Dev <br>
+					Frontend Dev <br>
+					Backend Dev <br>
+					Engineer Manager <br>
+					QA Engineer <br>
+					Dev Ops <br>
+					Software Architect <br>
 
                 @endcomponent
             </div>
