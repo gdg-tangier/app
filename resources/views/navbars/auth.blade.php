1d19c5e4a39f87342ddfa1614e4e36652ba67b45
Unknown <souhail.merroun@hotmail.com>
2018-03-20 03:59:52 +0000

    Navbar enhacement

--- resources/views/navbars/auth.blade.php
+++ resources/views/navbars/auth.blade.php
@@ -3,7 +3,21 @@
     @slot('left')
 
         <li><a class="nav-link" href="/home">Dashboard</a></li>
-        <li><a class="nav-link" href="{{ route('users.index') }}">Members</a></li>
+        <li><a class="nav-link" href="https://www.meetup.com/gdgtangier/events/" target="_blank">Events</a></li>
+        <li><a class="nav-link" href="/opportunities">Opportunities</a></li>
+        <li><a class="nav-link" href="/members">Members</a></li>
+        <li><a class="nav-link" href="/ask">Ask</a></li>
+        <li class="nav-item dropdown">
+            <a  class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                Inspiration <span  class="caret"></span>
+            </a>
+            
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="/moroccan-success-stories">Moroccan Success Stories</a>
+                <a class="dropdown-item" href="/interviews">Interviews</a>
+            </div>
+        </li>
+        <li><a class="nav-link" href="/share">Share</a></li>
 
         @if(auth()->user()->isAdmin())
 
@@ -13,6 +27,7 @@
                 </a>
                 
                 <div class="dropdown-menu">
+                    <a class="dropdown-item" href="{{ route('users.index') }}">Members</a>
                     <a class="dropdown-item" href="{{ route('admin.events.index') }}">Events</a>
                 </div>
             </li>
