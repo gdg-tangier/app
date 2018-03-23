e0c9e4508abda6726e32b2b2e5b6ab4153c724c8
Unknown <souhail.merroun@hotmail.com>
2018-03-23 13:29:37 +0000

    Backup from prod

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-# Personal Laravel 5.6 Boilerplate
+# App
 
-Sometimes I get ideas and I'd like to start prototyping mockups fast. I really like using Bootstrap UI for that. I also like the ability that php has to include files and finally I like Laravel ability to include views from routes. 
-
-Rather than starting from completly scratch, start from this which has already used components.
-
-If I don't need heroku, just revert the commit of the heroku config. 
-
-Enjoy ! 
-
-Installed Packages: 
-- (iseed)[https://github.com/orangehill/iseed]
--- database/seeds/EventUserTableSeeder.php
+++ database/seeds/EventUserTableSeeder.php
@@ -12,6 +12,93 @@ class EventUserTableSeeder extends Seeder
      */
     public function run()
     {
-            
+        
+
+        \DB::table('event_user')->delete();
+        
+        \DB::table('event_user')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'event_id' => 1,
+                'user_id' => 11,
+                'code' => 'xrZU4',
+                'state' => NULL,
+                'created_at' => '2018-03-20 04:05:50',
+                'updated_at' => '2018-03-20 04:05:50',
+            ),
+            1 => 
+            array (
+                'id' => 11,
+                'event_id' => 1,
+                'user_id' => 51,
+                'code' => 'oe0Kb',
+                'state' => NULL,
+                'created_at' => '2018-03-20 12:06:35',
+                'updated_at' => '2018-03-20 12:06:35',
+            ),
+            2 => 
+            array (
+                'id' => 21,
+                'event_id' => 1,
+                'user_id' => 61,
+                'code' => 'pzq6q',
+                'state' => NULL,
+                'created_at' => '2018-03-20 12:06:59',
+                'updated_at' => '2018-03-20 12:06:59',
+            ),
+            3 => 
+            array (
+                'id' => 31,
+                'event_id' => 1,
+                'user_id' => 81,
+                'code' => 'JmVfq',
+                'state' => NULL,
+                'created_at' => '2018-03-20 13:07:26',
+                'updated_at' => '2018-03-20 13:07:26',
+            ),
+            4 => 
+            array (
+                'id' => 41,
+                'event_id' => 1,
+                'user_id' => 91,
+                'code' => 'jjoOc',
+                'state' => NULL,
+                'created_at' => '2018-03-20 13:39:10',
+                'updated_at' => '2018-03-20 13:39:10',
+            ),
+            5 => 
+            array (
+                'id' => 51,
+                'event_id' => 1,
+                'user_id' => 101,
+                'code' => '68ZnJ',
+                'state' => NULL,
+                'created_at' => '2018-03-20 14:37:51',
+                'updated_at' => '2018-03-20 14:37:51',
+            ),
+            6 => 
+            array (
+                'id' => 61,
+                'event_id' => 1,
+                'user_id' => 111,
+                'code' => 'FWM2D',
+                'state' => NULL,
+                'created_at' => '2018-03-20 14:57:31',
+                'updated_at' => '2018-03-20 14:57:31',
+            ),
+            7 => 
+            array (
+                'id' => 71,
+                'event_id' => 1,
+                'user_id' => 121,
+                'code' => 'HxAg5',
+                'state' => NULL,
+                'created_at' => '2018-03-20 15:29:44',
+                'updated_at' => '2018-03-20 15:29:44',
+            ),
+        ));
+        
+        
     }
 }
