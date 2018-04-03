9393259a1ffb5e843bb15eacba5c8e3e4b96e4ed
Unknown <souhail.merroun@hotmail.com>
2018-04-03 08:01:16 +0100

    Setup testing environment

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => ':memory:',
             'prefix' => '',
         ],
 
