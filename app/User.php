730c399a655bab774f3c92c7dbecea8f0150a08a
Amrani Houssain <houssain@airbit.com>
2018-04-16 21:47:59 +0100

    Adopt PSR-2

--- app/User.php
+++ app/User.php
@@ -2,17 +2,24 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
 
+    /**
+     * @var array $guarded
+     */
     protected $guarded = [];
 
+    /**
+     * Check if user is admin.
+     * @return boolean
+     */
     public function isAdmin()
     {
-    	return $this->role === 'admin';
+        return $this->role === 'admin';
     }
 }
